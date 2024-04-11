7a7b8adecbaa8ec997e8258b6ff1d05ea8a5011d
JustGuys <158515115+JustGuys@users.noreply.github.com>
2024-04-11 14:02:16 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,1 @@
+if [[ -d .git ]] && [[ "1" == "1" ]]; then git pull; fi; if [[ ! -z "" ]]; then pip install -U --prefix .local ; fi; if [[ -f /home/container/${REQUIREMENTS_FILE} ]]; then pip install -U --prefix .local -r ${REQUIREMENTS_FILE}; fi; /usr/local/bin/python /home/container/main.py
